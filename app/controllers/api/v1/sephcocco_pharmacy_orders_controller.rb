ca6e6c5aeb15926cb7616743482f9e7fa97f6739
Zia-Ur-Rehman1 <ziakhanalone100@gmail.com>
2025-05-26 06:24:02 +0500

    Replicated Orders controller for pharmacy & resturant

---
+++ app/controllers/api/v1/sephcocco_pharmacy_orders_controller.rb
@@ -0,0 +1,32 @@
+# app/controllers/api/v1/sephcocco_pharmacy_orders_controller.rb
+class Api::V1::SephcoccoPharmacyOrdersController < ApplicationController
+  include Api::V1::Concerns::Orderable
+
+  private
+
+  def order_class
+    SephcoccoPharmacyOrder
+  end
+
+  def order_association
+    :sephcocco_pharmacy_orders
+  end
+
+  def order_params
+    if current_user.sephcocco_user_role.name == 'admin'
+      params.require(:sephcocco_pharmacy_order).permit(
+        :sephcocco_user_id,
+        :quantity,
+        :unit_price,
+        :total_price,
+        :shipping_cost,
+        :total_cost,
+        :status,
+        :order_number,
+        stages: []
+      )
+    else
+      params.require(:sephcocco_pharmacy_order).permit(:quantity)
+    end
+  end
+end
---
+++ app/controllers/api/v1/sephcocco_restaurant_orders_controller.rb
@@ -0,0 +1,32 @@
+# app/controllers/api/v1/sephcocco_restaurant_orders_controller.rb
+class Api::V1::SephcoccoRestaurantOrdersController < ApplicationController
+  include Api::V1::Concerns::Orderable
+
+  private
+
+  def order_class
+    SephcoccoRestaurantOrder
+  end
+
+  def order_association
+    :sephcocco_restaurant_orders
+  end
+
+  def order_params
+    if current_user.sephcocco_user_role.name == 'admin'
+      params.require(:sephcocco_restaurant_order).permit(
+        :sephcocco_user_id,
+        :quantity,
+        :unit_price,
+        :total_price,
+        :shipping_cost,
+        :total_cost,
+        :status,
+        :order_number,
+        stages: []
+      )
+    else
+      params.require(:sephcocco_restaurant_order).permit(:quantity)
+    end
+  end
+end
