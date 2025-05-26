c34622f5cd8fc15e84c597f69cff32f1bf06677c
Zia-Ur-Rehman1 <ziakhanalone100@gmail.com>
2025-05-26 06:43:14 +0500

    Model: Pharmacy & Resturant Order relations created

---
+++ app/models/sepcocco_restaurant_order.rb
@@ -0,0 +1,11 @@
+class SephcoccoRestuarantOrder < ApplicationRecord
+  include OrderCallbacks
+
+  belongs_to :sephcocco_restuarant_product
+  belongs_to :sephcocco_user
+
+  # Alias to standardize the method used in the concern
+  def product
+    sephcocco_restuarant_product
+  end
+end
