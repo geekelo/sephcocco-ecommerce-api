08d7ccdd9cf9cb4bdc8a89033014282af8c463ec
Eloghene <eloghene@bananacrystal.com>
2025-05-30 01:45:41 +0100

    Fix name space in model

--- app/models/lounge/sephcocco_lounge_product.rb
+++ app/models/lounge/sephcocco_lounge_product.rb
@@ -18,11 +18,11 @@ class Lounge::SephcoccoLoungeProduct < ApplicationRecord
   end
 
   def self.product_like_class
-    SephcoccoLoungeProductLike
+    Lounge::SephcoccoLoungeProductLike
   end
 
   def self.order_class
-    SephcoccoLoungeOrder
+    Lounge::SephcoccoLoungeOrder
   end
 
   def self.likes_association_name
