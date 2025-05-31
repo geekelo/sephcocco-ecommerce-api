07e3ccb1d115d4502e3a664bc50ec940654cd213
Eloghene <eloghene@bananacrystal.com>
2025-05-31 17:35:31 +0100

    seperate concerns for controller

---
+++ app/controllers/api/v1/concerns/message_controller_helper.rb
@@ -0,0 +1,91 @@
+module MessageControllerHelper
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :authenticate_user!
+  end
+
+  def index
+    messages = if admin?
+      message_class.pluck(:id, product_foreign_key, :created_at, :status)
+    else
+      current_user.send(user_association_name)
+                  .where(status: 'open')
+                  .pluck(:id, product_foreign_key, :created_at, :status)
+    end
+
+    render json: messages, each_serializer: serializer_class
+  end
+
+  def show
+    message = message_class.find(params[:id])
+    render json: message, serializer: user_serializer_class
+  end
+
+  def create
+    message = Lounge::Messages::CreateService.new(
+      user: current_user,
+      params: message_params,
+      product_id: message_params[product_param_key],
+      message_class: message_class
+    ).call
+
+    render json: message, serializer: serializer_class, status: :created
+  rescue ActiveRecord::RecordInvalid => e
+    render json: e.record.errors, status: :unprocessable_entity
+  end
+
+  def update
+    message = Lounge::Messages::UpdateService.new(
+      user: current_user,
+      message_id: params[:id],
+      params: message_params,
+      message_class: message_class
+    ).call
+
+    render json: message, serializer: serializer_class
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'Not authorized or message not found' }, status: :forbidden
+  rescue ActiveRecord::RecordInvalid => e
+    render json: e.record.errors, status: :unprocessable_entity
+  end
+
+  private
+
+  def admin?
+    current_user.sephcocco_user_role.name == 'admin'
+  end
+
+  def message_params
+    params.require(:message).permit(:chat, product_param_key)
+  end
+
+  # Abstract methods to override in each controller
+  def message_class
+    raise NotImplementedError, "Define `message_class` in your controller"
+  end
+
+  def product_param_key
+    raise NotImplementedError, "Define `product_param_key` in your controller"
+  end
+
+  def product_foreign_key
+    raise NotImplementedError, "Define `product_foreign_key` in your controller"
+  end
+
+  def user_association_name
+    raise NotImplementedError, "Define `user_association_name` in your controller"
+  end
+
+  def serializer_class
+    admin? ? admin_serializer_class : user_serializer_class
+  end
+
+  def admin_serializer_class
+    raise NotImplementedError, "Define `admin_serializer_class` in your controller"
+  end
+
+  def user_serializer_class
+    raise NotImplementedError, "Define `user_serializer_class` in your controller"
+  end
+end
--- app/controllers/api/v1/lounge/sephcocco_lounge_messages_controller.rb
+++ app/controllers/api/v1/lounge/sephcocco_lounge_messages_controller.rb
@@ -1,63 +1,29 @@
 class Api::V1::Lounge::SephcoccoLoungeMessagesController < ApplicationController
-  before_action :authenticate_user!
-
-  def index
-    messages = if current_user.sephcocco_user_role.name == 'admin'
-      Lounge::SephcoccoLoungeMessage.pluck(:id, :sephcocco_lounge_product_id, :created_at, :status)
-    else
-      current_user.send(:sephcocco_lounge_messages).where(status: 'open').pluck(:id, :sephcocco_lounge_product_id, :created_at, :status)
-    end
-
-    serializer = admin? ? Lounge::Admin::SephcoccoLoungeMessageSerializer : Lounge::User::SephcoccoLoungeMessageSerializer
-    render json: messages, each_serializer: serializer
-  end
-
-  def show
-    message = Lounge::SephcoccoLoungeMessage.find(params[:id])
-    render json: message, serializer: Lounge::User::SephcoccoLoungeMessageSerializer
-  end
-
-  def create
-    begin
-      @message = Lounge::Messages::CreateService.new(user: current_user, params: message_params, product_id: message_params[:sephcocco_lounge_product_id], message_class: Lounge::SephcoccoLoungeMessage).call
-      
-      if admin?
-        render json: @message, serializer: Lounge::Admin::SephcoccoLoungeMessageSerializer, status: :created
-      else
-        render json: @message, serializer: Lounge::User::SephcoccoLoungeMessageSerializer, status: :created
-      end
-    rescue ActiveRecord::RecordInvalid => e
-      render json: e.record.errors, status: :unprocessable_entity
-    end
-  end
-
-  def update
-    begin
-      @message = Lounge::Messages::UpdateService.new(
-        user: current_user,
-        message_id: params[:id],
-        params: message_params,
-        message_class: Lounge::SephcoccoLoungeMessage
-      ).call
-      if admin?
-        render json: @message, serializer: Lounge::Admin::SephcoccoLoungeMessageSerializer
-      else
-        render json: @message, serializer: Lounge::User::SephcoccoLoungeMessageSerializer
-      end
-    rescue ActiveRecord::RecordNotFound
-      render json: { error: 'Not authorized or message not found' }, status: :forbidden
-    rescue ActiveRecord::RecordInvalid => e
-      render json: e.record.errors, status: :unprocessable_entity
-    end
-  end
+  include MessageControllerHelper
 
   private
 
-  def admin?
-    current_user.sephcocco_user_role.name == 'admin'
+  def message_class
+    Lounge::SephcoccoLoungeMessage
   end
 
-  def message_params
-    params.require(:message).permit(:chat, :sephcocco_lounge_product_id)
+  def product_param_key
+    :sephcocco_lounge_product_id
+  end
+
+  def product_foreign_key
+    :sephcocco_lounge_product_id
+  end
+
+  def user_association_name
+    :sephcocco_lounge_messages
+  end
+
+  def admin_serializer_class
+    Lounge::Admin::SephcoccoLoungeMessageSerializer
+  end
+
+  def user_serializer_class
+    Lounge::User::SephcoccoLoungeMessageSerializer
   end
 end
