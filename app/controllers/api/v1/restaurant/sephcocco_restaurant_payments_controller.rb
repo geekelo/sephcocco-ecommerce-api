d13df6b57965a6b72d0b53fec18b2aa30977ee8d
Terry Oko-odion <terryokoodion@gmail.com>
2025-06-01 12:02:26 +0100

    Added payments controller for Restaurant

---
+++ app/controllers/api/v1/restaurant/sephcocco_restaurant_payments_controller.rb
@@ -0,0 +1,24 @@
+class Api::V1::Restaurant::SephcoccoRestaurantPaymentsController < ApplicationController
+  include Api::V1::Concerns::PaymentsControllerHelper
+
+  private
+
+  def payment_class
+    SephcoccoRestaurantPayment
+  end
+
+  def payment_association
+    :sephcocco_restaurant_payments
+  end
+
+  def payment_params
+    params.require(:sephcocco_restaurant_payment).permit(
+      :sephcocco_user_id,
+      :order_id,
+      :amount,
+      :payment_method,
+      :status,
+      :transaction_id
+    )
+  end
+end
