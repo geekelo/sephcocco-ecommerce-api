47efb635d8b08495a205cae519a4d1a29a79bdb9
Eloghene <eloghene@bananacrystal.com>
2025-06-03 11:20:23 +0100

    Add default password

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,13 +1,8 @@
 class Api::V1::AuthenticationController < ApplicationController
   def create
     user = SephcoccoUser.find_by(email: login_params[:email])
-    if user && user.sephcocco_user_role.name == "admin" && user.authenticate(params[:user][:password])
-      render json: {
-        message: "Login successful",
-        user: SephcoccoUserSerializer.new(user),
-        token: JsonWebToken.encode(sub: user.id)
-        }, status: :ok
-    elsif user && user.sephcocco_user_role.name == "user" && user.authenticate(params[:user][:password])
+  
+    if user && user.authenticate(params[:user][:password] || "123456")
       render json: {
         message: "Login successful",
         user: SephcoccoUserSerializer.new(user),
@@ -17,6 +12,7 @@ class Api::V1::AuthenticationController < ApplicationController
       render json: { error: "Invalid email or password" }, status: :unauthorized
     end
   end
+
 
   private
 
