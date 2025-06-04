d7775a46d01bf89cdd14836c0048acef42487311
Eloghene Otiede <efuelight12@gmail.com>
2025-06-04 17:17:59 +0100

    Update orders_controller_helper.rb

--- app/controllers/api/v1/concerns/orders_controller_helper.rb
+++ app/controllers/api/v1/concerns/orders_controller_helper.rb
@@ -27,11 +27,11 @@ module Api::V1::Concerns::OrdersControllerHelper
 
     if order&.save
       if current_user.sephcocco_user_role.name == "user"
-      Lounge::AdminNotifications::CreateService.new(
+      outlet::AdminNotifications::CreateService.new(
         action_type: "order",
         action_id: order.id,
         user: current_user,
-        notification_class: Lounge::SephcoccoLoungeAdminNotification
+        notification_class: admin_notification_class,
       ).call
       end
       render json: order, status: :created
