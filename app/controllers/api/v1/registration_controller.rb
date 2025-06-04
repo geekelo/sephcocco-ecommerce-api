cb05bb1c94c7c83ed62b34f7970cacccf0856ce2
Eloghene <eloghene@bananacrystal.com>
2025-06-04 14:21:47 +0100

    Update registration to handle role assignment

--- app/controllers/api/v1/registration_controller.rb
+++ app/controllers/api/v1/registration_controller.rb
@@ -1,16 +1,19 @@
 class Api::V1::RegistrationController < ApplicationController
   def create
+     user_role_id = SephcoccoUserRole.find_by(name: params[:user][:role])&.id unless params[:user][:role].blank?
+
     if email_already_exists?
-      render json: { error: "Email already exists" }, status: :unprocessable_entity
+      render json: { error: "Email is already registered with us" }, status: :unprocessable_entity
     else
-      password = params[:event_user][:password].presence || "123456"
-      password_confirmation = params[:event_user][:password_confirmation].presence || "123456"
-  
+      password = params[:user][:password].presence || "123456"
+      password_confirmation = params[:user][:password_confirmation].presence || "123456"
+
       user = SephcoccoUser.new(user_params.merge(
         password: password,
-        password_confirmation: password_confirmation
+        password_confirmation: password_confirmation,
+        sephcocco_user_role_id: user_role_id || SephcoccoUserRole.find_by(name: "user")&.id # Default to 'user' role if not specified
       ))
-  
+
       if user.save
         render json: { message: "User created successfully" }, status: :created
       else
@@ -23,10 +26,10 @@ class Api::V1::RegistrationController < ApplicationController
   private
 
   def user_params
-    params.require(:event_user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation, :name, :address, :phone_number, :whatsapp_number, :role)
   end
 
   def email_already_exists?
-    SephcoccoUser.exists?(email: params[:event_user][:email])
+    SephcoccoUser.exists?(email: params[:user][:email])
   end
 end
