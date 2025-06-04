82b4a9075649ebea4049dc980f9c4bd5576607bd
Eloghene Otiede <efuelight12@gmail.com>
2025-06-04 22:23:48 +0100

    Delete db/migrate/20250604140841_create_restaurant_sephcocco_restaurant_admin_notifications.rb

--- db/migrate/20250604140841_create_restaurant_sephcocco_restaurant_admin_notifications.rb
+++
@@ -1,7 +0,0 @@
-class CreateRestaurantSephcoccoRestaurantAdminNotifications < ActiveRecord::Migration[7.2]
-  def change
-    create_table :restaurant_sephcocco_restaurant_admin_notifications do |t|
-      t.timestamps
-    end
-  end
-end
