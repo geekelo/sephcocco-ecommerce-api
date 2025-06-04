2903634348b78c63d8275f57bba058ade4310a5f
Eloghene <eloghene@bananacrystal.com>
2025-06-04 16:25:25 +0100

    Add outlets

--- app/serializers/sephcocco_user_serializer.rb
+++ app/serializers/sephcocco_user_serializer.rb
@@ -1,7 +1,15 @@
 class SephcoccoUserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :address, :phone_number, :whatsapp_number, :role, :created_at, :updated_at
+  attributes :id, :name, :email, :address, :phone_number, :whatsapp_number, :role, :outlets, :created_at, :updated_at
 
   def role
     object&.sephcocco_user_role&.name
+  end
+
+  def outlets
+    object&.sephcocco_outlets&.map do |outlet|
+      {
+        name: outlet.name
+      }
+    end || []
   end
 end
