2978239ae22ae656b4e5083dd96ef72831ac1570
gids-dadi <gideons564@gmail.com>
2025-06-05 15:21:57 +0100

    Add the restaurant message controllers and serializers

---
+++ app/serializers/restaurant/user/sephcocco_restaurant_message_serializer.rb
@@ -0,0 +1,8 @@
+
+class Lounge::User::SephcoccoRestaurantMessageSerializer < ActiveModel::Serializer
+  attributes :id, :chats, :status, :user_id
+
+  def user_id
+    object.sephcocco_user_id
+  end
+end
