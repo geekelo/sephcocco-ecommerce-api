3ffd605c4625d285b9c771f7eb75ca487016d358
Terry Oko-odion <terryokoodion@gmail.com>
2025-06-08 22:30:05 +0100

    faq models for pharmacy and restaurant

---
+++ app/models/api/v1/pharmacy/sephcocco_pharmacy_faq.rb
@@ -0,0 +1,3 @@
+class Api::V1::Pharmacy::SephcoccoPharmacyFaq < ApplicationRecord
+  belongs_to :sephcocco_pharmacy_faq_category
+end
---
+++ app/models/api/v1/restaurant/sephcocco_restaurant_faq.rb
@@ -0,0 +1,3 @@
+class Api::V1::Restaurant::SephcoccoRestaurantFaq < ApplicationRecord
+  belongs_to :sephcocco_restaurant_faq_category
+end
