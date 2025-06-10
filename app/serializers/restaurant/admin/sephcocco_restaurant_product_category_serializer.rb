d7efe268703884dc3125f4820a77e15708ba2dde
Eloghene <eloghene@bananacrystal.com>
2025-06-10 16:35:48 +0100

    Test

--- app/serializers/restaurant/admin/sephcocco_restaurant_product_category_serializer.rb
+++ app/serializers/restaurant/admin/sephcocco_restaurant_product_category_serializer.rb
@@ -2,15 +2,7 @@ class Restaurant::Admin::SephcoccoRestaurantProductCategorySerializer < ActiveMo
   attributes  :id,
               :name,
               :slug,
-              :products,
               :description,
               :created_at,
               :updated_at
-
-
-  def products
-    object.sephcocco_restaurant_products.map do |product|
-      Restaurant::Admin::SephcoccoRestaurantProductSerializer.new(product).as_json
-    end
-  end
 end
