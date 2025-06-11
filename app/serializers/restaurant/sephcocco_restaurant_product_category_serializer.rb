a0e8067f6e4e78cecbf0d2969812a202d976f573
Eloghene <eloghene@bananacrystal.com>
2025-06-11 09:49:08 +0100

    Test cors

--- app/serializers/restaurant/sephcocco_restaurant_product_category_serializer.rb
+++ app/serializers/restaurant/sephcocco_restaurant_product_category_serializer.rb
@@ -3,5 +3,10 @@ class Restaurant::SephcoccoRestaurantProductCategorySerializer < ActiveModel::Se
               :name,
               :description,
               :created_at,
-              :updated_at
+              :updated_at,
+              :total_products,
+
+  def total_products
+    object&.sephcocco_restaurant_products.count || 0
+  end
 end
