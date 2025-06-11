30755a40822d15d54bc02187ccf094bc540ca76b
Eloghene <eloghene@bananacrystal.com>
2025-06-11 09:06:09 +0100

    Test cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins [ "http://localhost:3000", "https://sephcocco-eccomerce-admin.vercel.app", "https://sephcocco-lounge-user.vercel.app", "http://localhost:5173" ] # Add your frontend origin(s)
 
-    resource "/api/*",
+    resource "*",
       headers: :any,
       methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
       credentials: true
