b8906cbe2cec2abb7a5a0e7a3df7505cb58a5cd5
Eloghene <eloghene@bananacrystal.com>
2025-06-11 10:19:54 +0100

    Test category join table

--- app/serializers/restaurant/sephcocco_restaurant_product_category_serializer.rb
+++ app/serializers/restaurant/sephcocco_restaurant_product_category_serializer.rb
@@ -7,6 +7,6 @@ class Restaurant::SephcoccoRestaurantProductCategorySerializer < ActiveModel::Se
               :total_products,
 
   def total_products
-    object&.restaurant_products.count || 0
+    object&.sephcocco_restaurant_products&.count || 0
   end
 end
