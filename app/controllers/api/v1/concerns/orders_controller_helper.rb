e41a3804be831294040927e9a6e516850cad2be6
Eloghene <eloghene@bananacrystal.com>
2025-06-24 16:20:51 +0100

    Fix issues with creating order

--- app/controllers/api/v1/concerns/orders_controller_helper.rb
+++ app/controllers/api/v1/concerns/orders_controller_helper.rb
@@ -19,7 +19,7 @@ module Api::V1::Concerns::OrdersControllerHelper
   end
 
   def create
-    unit_price = params[:unit_price] || product_class.find(order_params[:sephcocco_product_id]).price
+    unit_price = params[:unit_price] || product_class.find(order_params[:sephcocco_pharmacy_product_id]).price || product_class.find(order_params[:sephcocco_restaurant_product_id]).price || product_class.find(order_params[:sephcocco_lounge_product_id]).price
     if current_user&.sephcocco_user_role&.name == "admin"
       order = @customer&.send(order_association).new(order_params.merge(unit_price: unit_price))
     else
