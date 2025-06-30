4540c11b3c7500d924107b869afdfa4fa25737d4
Eloghene <eloghene@bananacrystal.com>
2025-06-30 13:29:29 +0100

    Fix syntax error

--- app/models/lounge/sephcocco_lounge_admin_notification.rb
+++ app/models/lounge/sephcocco_lounge_admin_notification.rb
@@ -1,16 +1,3 @@
 class Lounge::SephcoccoLoungeAdminNotification < ApplicationRecord
   belongs_to :sephcocco_user
-
-  after_update :switch_visibility
-
-  private
-
-  def switch_visibility
-    if viewed
-      # Logic to switch visibility
-      self.update(visible: false)
-    else
-      self.update(visible: true)
-    end
-  end
 end
--- app/models/restaurant/sephcocco_restaurant_admin_notification.rb
+++ app/models/restaurant/sephcocco_restaurant_admin_notification.rb
@@ -1,17 +1,4 @@
 
 class Restaurant::SephcoccoRestaurantAdminNotification < ApplicationRecord
   belongs_to :sephcocco_user
-
-  after_update :switch_visibility
-
-  private
-
-  def switch_visibility
-    if viewed
-      # Logic to switch visibility
-      self.update(visible: false)
-    else
-      self.update(visible: true)
-    end
-  end
 end
