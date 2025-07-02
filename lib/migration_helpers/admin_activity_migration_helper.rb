77e654ac8e412fd8c36de5faa9e750bd396bc27f
Eloghene <eloghene@bananacrystal.com>
2025-07-02 11:27:28 +0100

    Fix FAQ

--- lib/migration_helpers/admin_activity_migration_helper.rb
+++ lib/migration_helpers/admin_activity_migration_helper.rb
@@ -2,7 +2,7 @@ module MigrationHelpers
   module AdminActivityMigrationHelper
     def create_admin_activity_table(table_name)
       create_table table_name, id: :uuid do |t|
-        t.references :sephcocco_user, null: false, foreign_key: true
+        t.references :sephcocco_user, type: :uuid, null: false, foreign_key: true
         t.string :activity_type
         t.string :activity_name
         t.string :activity_description
