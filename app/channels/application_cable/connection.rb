d0f622ca2a1090fbf356dbeeec18367fe91de105
Eloghene <eloghene@bananacrystal.com>
2025-07-21 15:29:17 +0100

    Fix cable authentication

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -12,16 +12,34 @@ module ApplicationCable
       # Get the token from the request
       token = request.params[:token] || request.headers['Authorization']&.split(' ')&.last
       
+      Rails.logger.info "ActionCable: Attempting connection with token: #{token ? 'Present' : 'Missing'}"
+      
       if token
         begin
           # Decode the JWT token
           decoded_token = JsonWebToken.decode(token)
-          user_id = decoded_token[:user_id]
-          SephcoccoUser.find(user_id)
-        rescue JWT::DecodeError, ActiveRecord::RecordNotFound
+          
+          if decoded_token && decoded_token["sub"]
+            user_id = decoded_token["sub"]
+            user = SephcoccoUser.find(user_id)
+            Rails.logger.info "ActionCable: Successfully authenticated user: #{user.email}"
+            user
+          else
+            Rails.logger.error "ActionCable: Invalid token structure"
+            reject_unauthorized_connection
+          end
+        rescue JWT::DecodeError => e
+          Rails.logger.error "ActionCable: JWT decode error: #{e.message}"
+          reject_unauthorized_connection
+        rescue ActiveRecord::RecordNotFound => e
+          Rails.logger.error "ActionCable: User not found: #{e.message}"
+          reject_unauthorized_connection
+        rescue => e
+          Rails.logger.error "ActionCable: Unexpected error: #{e.message}"
           reject_unauthorized_connection
         end
       else
+        Rails.logger.error "ActionCable: No token provided"
         reject_unauthorized_connection
       end
     end
