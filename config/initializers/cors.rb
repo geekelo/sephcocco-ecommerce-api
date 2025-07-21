e7114e66c9bd4d395dc52213f133468bf0f5b04c
Eloghene <eloghene@bananacrystal.com>
2025-07-21 15:04:31 +0100

    Add cable to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,5 +6,10 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       headers: :any,
       methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
       credentials: true
+    
+    resource "/cable",
+      headers: :any,
+      methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
+      credentials: true
   end
 end
