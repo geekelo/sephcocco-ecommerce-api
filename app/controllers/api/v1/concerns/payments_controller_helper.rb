ac2a6c2948b3e2af0e2112a6b489fc711e220e2e
Eloghene <eloghene@bananacrystal.com>
2025-08-14 19:42:50 +0100

    Find payment by reference

--- app/controllers/api/v1/concerns/payments_controller_helper.rb
+++ app/controllers/api/v1/concerns/payments_controller_helper.rb
@@ -226,7 +226,7 @@ module Api::V1::Concerns::PaymentsControllerHelper
     if body['data'] && body['data']['status'] == 'success'
       # ✅ Payment is verified and successful
       # Find the payment by reference and update it
-      payment = payment_class.find_by(id: params[:id])
+      payment = payment_class.find_by(reference: reference)
       
       if payment
         payment.update(status: "payment confirmed")
