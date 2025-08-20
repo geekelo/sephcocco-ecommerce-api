c198ee23d502aa98d918c2ad7834e169cacd995e
Eloghene <eloghene@bananacrystal.com>
2025-08-20 04:25:54 +0100

    Display total shipped by rider

--- app/serializers/sephcocco_user_serializer.rb
+++ app/serializers/sephcocco_user_serializer.rb
@@ -1,5 +1,5 @@
 class SephcoccoUserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :address, :payment_ref, :phone_number, :whatsapp_number, :role, :outlets, :suspended, :last_login_at, :created_at, :updated_at, :total_orders
+  attributes :id, :name, :email, :address, :payment_ref, :phone_number, :whatsapp_number, :role, :outlets, :suspended, :last_login_at, :created_at, :updated_at, :total_orders, :total_shippings
 
   def role
     object&.sephcocco_user_role&.name
@@ -19,4 +19,15 @@ class SephcoccoUserSerializer < ActiveModel::Serializer
     total_orders
   end
 
+  def total_shippings
+    if object.sephcocco_user_role.name == "admin"
+    pharmacy_shippings = Pharmacy::SephcoccoPharmacyShipping.where(sephcocco_user_id: object.id, status: "delivered")
+    restaurant_shippings = Restaurant::SephcoccoRestaurantShipping.where(sephcocco_user_id: object.id, status: "delivered")
+      lounge_shippings = Lounge::SephcoccoLoungeShipping.where(sephcocco_user_id: object.id, status: "delivered")
+      total_shippings = pharmacy_shippings.count + restaurant_shippings.count + lounge_shippings.count
+      total_shippings
+    else
+      total_shippings = "not a rider"
+    end
+  end
 end
