740f27aa7cb0bd2e95513ac4a903d39fa2ed9f05
Eloghene <eloghene@bananacrystal.com>
2025-09-14 09:41:57 +0100

    Fix search terms on payments filter

--- app/controllers/api/v1/concerns/payments_controller_helper.rb
+++ app/controllers/api/v1/concerns/payments_controller_helper.rb
@@ -46,7 +46,7 @@ module Api::V1::Concerns::PaymentsControllerHelper
       if params[:filter][:search_terms].present?
         term = "%#{params[:filter][:search_terms]}%"
         payments = payments.joins(:sephcocco_user).where(
-          "CAST(#{payment_class.table_name}.amount AS TEXT) ILIKE :term OR #{payment_class.table_name}.transaction_id ILIKE :term OR sephcocco_users.name ILIKE :term OR orders ILIKE :term",
+          "CAST(#{payment_class.table_name}.amount AS TEXT) ILIKE :term OR #{payment_class.table_name}.transaction_id ILIKE :term OR sephcocco_users.name ILIKE :term OR orders::text ILIKE :term",
           term: term
         )
       end
