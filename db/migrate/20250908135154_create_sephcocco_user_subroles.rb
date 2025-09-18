15a0e46136bb54011037e06f8aa79e3f11d15883
Eloghene <eloghene@bananacrystal.com>
2025-09-18 09:57:04 +0100

    Fix subroles migration

--- db/migrate/20250908135154_create_sephcocco_user_subroles.rb
+++ db/migrate/20250908135154_create_sephcocco_user_subroles.rb
@@ -5,12 +5,5 @@ class CreateSephcoccoUserSubroles < ActiveRecord::Migration[7.2]
       t.string :description
       t.timestamps
     end
-
-    add_reference :sephcocco_users, :sephcocco_user_subrole, type: :uuid, foreign_key: true
   end
-
-  # def down
-  #   drop_table :sephcocco_user_subroles
-  #    remove_reference :sephcocco_users, :sephcocco_user_subrole
-  # end
 end
