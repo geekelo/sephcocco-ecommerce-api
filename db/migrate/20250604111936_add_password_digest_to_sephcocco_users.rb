4b44db78aa9db63f9c74d467203566539f847d84
Eloghene <eloghene@bananacrystal.com>
2025-09-18 09:45:54 +0100

    Fix migration issues

--- db/migrate/20250604111936_add_password_digest_to_sephcocco_users.rb
+++ db/migrate/20250604111936_add_password_digest_to_sephcocco_users.rb
@@ -1,9 +1,9 @@
 class AddPasswordDigestToSephcoccoUsers < ActiveRecord::Migration[7.2]
-def up
-    add_column :sephcocco_users, :password_digest, :string, null: true
-  end
+# def up
+#     add_column :sephcocco_users, :password_digest, :string, null: true
+#   end
 
-  def down
-    remove_column :sephcocco_users, :password_digest
-  end
+#   def down
+#     remove_column :sephcocco_users, :password_digest
+#   end
 end
