7cdb93d3d2312d4656a8df186bc73ae12f69e069
Eloghene <eloghene@bananacrystal.com>
2025-09-18 23:48:16 +0100

    Update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins [ "http://localhost:3000", "https://sephcocco-eccomerce-admin.vercel.app", "https://sephcocco-lounge-user.vercel.app", "http://localhost:5173" ] # Add your frontend origin(s)
+    origins [ "https://app.sephcocco.com.ng", "https://manage.sephcocco.com.ng", "http://localhost:3000", "https://sephcocco-eccomerce-admin.vercel.app", "https://sephcocco-lounge-user.vercel.app", "http://localhost:5173" ] # Add your frontend origin(s)
 
     resource "/api/*",
       headers: :any,
