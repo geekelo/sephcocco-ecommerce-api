0bfa582892ab302102c1da0ae42290bc54bbe023
Eloghene <eloghene@bananacrystal.com>
2025-09-25 12:40:25 +0100

    Fix migration

--- db/migrate/20250925103332_rename_lounge_product_categories_join_table.rb
+++ db/migrate/20250925103332_rename_lounge_product_categories_join_table.rb
@@ -1,12 +1,12 @@
 # db/migrate/20250925103332_rename_lounge_product_categories_join_table.rb
 class RenameLoungeProductCategoriesJoinTable < ActiveRecord::Migration[7.2]
   def up
-    rename_table :sephcocco_lounge_product_categories_lounge_products, 
+    rename_table :sephcocco_lounge_product_categories_products, 
                  :lounge_product_categories_lounge_products
   end
 
   def down
     rename_table :lounge_product_categories_lounge_products,
-                 :sephcocco_lounge_product_categories_lounge_products
+                 :sephcocco_lounge_product_categories_products
   end
 end
