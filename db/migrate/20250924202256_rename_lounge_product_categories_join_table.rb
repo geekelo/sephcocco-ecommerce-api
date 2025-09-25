6eb804b76a42255aae57b7d7ba811e3cb6f9f768
Eloghene <eloghene@bananacrystal.com>
2025-09-25 09:59:36 +0100

    Fix bugs

--- db/migrate/20250924202256_rename_lounge_product_categories_join_table.rb
+++ db/migrate/20250924202256_rename_lounge_product_categories_join_table.rb
@@ -1,13 +1,13 @@
 class RenameLoungeProductCategoriesJoinTable < ActiveRecord::Migration[7.2]
   def up
     # Rename the table from the actual database name to what the model expects
-    rename_table :sephcocco_lounge_product_categories_sephcocco_lounge_products, 
+    rename_table :lounge_product_categories_products,
                  :lounge_product_categories_lounge_products
   end
 
   def down
     # Reverse the rename
     rename_table :lounge_product_categories_lounge_products,
-                 :sephcocco_lounge_product_categories_sephcocco_lounge_products
+                 :lounge_product_categories_products
   end
 end
