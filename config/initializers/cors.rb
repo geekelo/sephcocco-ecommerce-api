533b8ac8fea3890ba5f9fbce53bb26e7e5db0381
Eloghene <eloghene@bananacrystal.com>
2025-09-26 12:42:15 +0100

    fix cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins [ "https://sephcocco-lounge-user-git-predeploy-geekelos-projects.vercel.app", "https://sephcocco-eccomerce-admin-git-predeploy-geekelos-projects.vercel.app", "https://app.sephcocco.com.ng", "https://manage.sephcocco.com.ng", "http://localhost:3000", "https://sephcocco-eccomerce-admin.vercel.app", "https://sephcocco-lounge-user.vercel.app", "http://localhost:5173" ] # Add your frontend origin(s)
+    origins [ "https://sephcocco-eccomerce-admin-git-predeploy-geekelos-projects.vercel.app", "https://sephcocco-lounge-user-git-predeploy-geekelos-projects.vercel.app", "https://sephcocco-eccomerce-admin-git-predeploy-geekelos-projects.vercel.app/", "https://app.sephcocco.com.ng", "https://manage.sephcocco.com.ng", "http://localhost:3000", "https://sephcocco-eccomerce-admin.vercel.app", "https://sephcocco-lounge-user.vercel.app", "http://localhost:5173" ] # Add your frontend origin(s)
 
     resource "/api/*",
       headers: :any,
@@ -11,6 +11,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       headers: :any,
       methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
       credentials: true
-      
+
   end
 end
