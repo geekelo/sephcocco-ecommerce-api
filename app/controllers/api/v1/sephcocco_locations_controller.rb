0669c9d014b13b8382946b402657929bf135eabc
Eloghene <eloghene@bananacrystal.com>
2025-10-13 00:01:47 +0100

    Implement location filtering and pagination in SephcoccoLocationsController; add SephcoccoLocationSerializer for consistent JSON responses.

--- app/controllers/api/v1/sephcocco_locations_controller.rb
+++ app/controllers/api/v1/sephcocco_locations_controller.rb
@@ -4,31 +4,55 @@ class Api::V1::SephcoccoLocationsController < ApplicationController
 
   def index
     @locations = SephcoccoLocation.all
-    render json: @locations
+
+    # Apply filters if they exist
+    if params[:filter].present?
+      if params[:filter][:search_terms].present?
+        @locations = @locations.where("location ILIKE ?", "location_price ILIKE ?", "%#{params[:filter][:search_terms]}%", "%#{params[:filter][:search_terms]}%")
+      end
+      if params[:filter][:start_date].present? && params[:filter][:end_date].present?
+        @locations = @locations.where(created_at: params[:filter][:start_date]..params[:filter][:end_date])
+      elsif params[:filter][:start_date].present?
+        @locations = @locations.where(created_at: params[:filter][:start_date]..Time.current)
+      elsif params[:filter][:end_date].present?
+        @locations = @locations.where(created_at: Time.current..params[:filter][:end_date])
+      end
+    end
+
+    @locations = @locations.order(created_at: :desc)
+    @locations = @locations.page(params[:page]).per(params[:per_page] || 20)
+
+    render json: {
+      locations: ActiveModelSerializers::SerializableResource.new(@locations, each_serializer: SephcoccoLocationSerializer).as_json,
+      meta: {
+        total_count: @locations.total_count,
+        total_pages: @locations.total_pages,
+        current_page: @locations.current_page
+      }
+    }
   end
 
   def create
     @location = SephcoccoLocation.new(location_params)
     if @location.save
-      render json: @location, status: :created
+      render json: @location, serializer: SephcoccoLocationSerializer, status: :created
     else
       render json: @location.errors, status: :unprocessable_entity
     end
   end
 
   def update
-    @location = SephcoccoLocation.find(params[:id])
     @location.update(location_params)
     if @location.save
-      render json: @location, status: :ok
+      render json: @location, serializer: SephcoccoLocationSerializer, status: :ok
     else
       render json: @location.errors, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @location = SephcoccoLocation.find(params[:id])
     @location.destroy
+    render json: { message: "Location deleted successfully" }, status: :ok
   end
 
   private
---
+++ app/serializers/sephcocco_location_serializer.rb
@@ -0,0 +1,3 @@
+class SephcoccoLocationSerializer < ActiveModel::Serializer
+  attributes :id, :location, :location_price, :created_at, :updated_at
+end
