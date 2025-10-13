bc52c9e15f03511ea80efb1e05070ac5a1d636bc
Eloghene <eloghene@bananacrystal.com>
2025-10-13 16:59:41 +0100

    Refactor vendor migration to include migration helpers and update comments for clarity on vendor table creation and deletion.

--- db/migrate/20251013083938_create_sephcocco_vendors.rb
+++ db/migrate/20251013083938_create_sephcocco_vendors.rb
@@ -1,13 +1,17 @@
+require_relative '../../lib/migration_helpers/vendor_migration_helper'
+
 class CreateSephcoccoVendors < ActiveRecord::Migration[7.2]
+  include MigrationHelpers::VendorMigrationHelper
+
   def up
-    # Create department tables for all three outlets
+    # Create vendor tables for all three outlets
     create_vendor_table(prefix: 'lounge')
     create_vendor_table(prefix: 'pharmacy')
     create_vendor_table(prefix: 'restaurant')
   end
 
   def down
-    # Drop department tables for all three outlets
+    # Drop vendor tables for all three outlets
     drop_vendor_table(prefix: 'lounge')
     drop_vendor_table(prefix: 'pharmacy')
     drop_vendor_table(prefix: 'restaurant')
