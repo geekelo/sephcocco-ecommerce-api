82042bab7469eb7f7d9334cfc2818882f2d38ba1
Eloghene <eloghene@bananacrystal.com>
2026-03-30 15:03:05 +0100

    Refactor order creation logic in OrdersControllerHelper to use current_user instead of @current_user, and update admin user checks to exclude 'waiters' subrole, improving order handling and role-based access.

--- app/controllers/api/v1/concerns/orders_controller_helper.rb
+++ app/controllers/api/v1/concerns/orders_controller_helper.rb
@@ -396,7 +396,7 @@ module Api::V1::Concerns::OrdersControllerHelper
       end
       
       unit_price = current_product.price
-      order = @current_user.send(order_association).new(unit_price: unit_price, quantity: product[:quantity].to_i, address: waiters_order_params[:address], additional_notes: waiters_order_params[:additional_notes])
+      order = current_user.send(order_association).new(unit_price: unit_price, quantity: product[:quantity].to_i, address: waiters_order_params[:address], additional_notes: waiters_order_params[:additional_notes])
       order.set_order_total(unit_price, product[:quantity].to_i)
       order.save!
       current_product.increment!(:likes)
@@ -431,7 +431,7 @@ module Api::V1::Concerns::OrdersControllerHelper
     Rails.logger.info "set_customer - user_role: #{current_user&.sephcocco_user_role&.name}"
     Rails.logger.info "set_customer - admin?: #{admin?}"
     
-    if admin?
+    if admin? && current_user.sephcocco_user_subrole.name != "waiters"
       # For admin users, get customer from order params
       customer_id = order_params[:sephcocco_user_id] || current_user.id
       if customer_id.blank?
